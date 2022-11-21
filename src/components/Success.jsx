8b444b84679766ca246a984e8493d8b47c1ac8cd
Artem <as.skachko@gmail.com>
2022-11-21 21:42:03 +0200

    gh-pages fix 4

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -4,7 +4,7 @@ const homepage = 'https://artemskachko.github.io/react_list';
 export const Success = ({ count }) => {
   return (
     <div class="success-block">
-      <img src={`${homepage}/assets/success.svg"`} alt="Success" />
+      <img src={`${homepage}/assets/success.svg`} alt="Success" />
       <h3>Успешно!</h3>
       <p>Всем {count} пользователям отправлено приглашение.</p>
       <button onClick={()=>window.location.reload()} className="send-invite-btn">Назад</button>
